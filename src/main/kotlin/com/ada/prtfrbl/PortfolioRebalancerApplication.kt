adadb3a037fc454cb3346b2d9a1a823cbbf521ba
Alexander Dadiani <alexdadiani1994@gmail.com>
2020-07-17 12:36:46 +0200

    Initial Commit

---
+++ src/main/kotlin/com/ada/prtfrbl/PortfolioRebalancerApplication.kt
@@ -0,0 +1,11 @@
+package com.ada.prtfrbl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioRebalancerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioRebalancerApplication>(*args)
+}
---
+++ src/test/kotlin/com/ada/prtfrbl/PortfolioRebalancerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ada.prtfrbl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioRebalancerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
