c547348914a6df2ae1b9685270a40de770dd639c
Alexander Dadiani <alexdadiani1994@gmail.com>
2020-07-19 16:27:03 +0200

    Add batchPost config

--- src/main/kotlin/com/ada/portfolioRebalancer/PortfolioRebalancerApplication.kt
+++ src/main/kotlin/com/ada/portfolioRebalancer/PortfolioRebalancerApplication.kt
@@ -4,6 +4,9 @@ import com.ada.portfolioRebalancer.csvImporter.Importer
 import com.ada.portfolioRebalancer.fpsHandler.FpsHandler
 import com.ada.portfolioRebalancer.strategyMatcher.StrategyMatcher
 import com.github.doyaaaaaken.kotlincsv.client.CsvReader
+import com.natpryce.konfig.ConfigurationProperties
+import com.natpryce.konfig.Key
+import com.natpryce.konfig.intType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -12,11 +15,15 @@ class PortfolioRebalancerApplication
 
 fun main(args: Array<String>) {
     runApplication<PortfolioRebalancerApplication>(*args)
+
+    val batchPost = Key("batchPost", intType)
+    val config = ConfigurationProperties.fromResource("application.properties")
+
     val importer = Importer(CsvReader())
     importer.import()
     val strategyMatcher = StrategyMatcher(importer.importedCustomers, importer.importedStrategies)
     val matchedCustomers = strategyMatcher.match()
     val fpsHandler = FpsHandler("http://localhost:8080/fps/", "execute", "customer", matchedCustomers)
     fpsHandler.calculateTrades()
-    fpsHandler.rebalancePortfolios()
+    fpsHandler.rebalancePortfolios(config[batchPost])
 }
